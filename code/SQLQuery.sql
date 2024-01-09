d1f8e758b049115beeeb8801440b3a03c05c0a6e
roliveira-9 <rafa.r.oliveira.rrdo@gmail.com>
2024-01-09 20:58:48 -0300

    Update Project

---
+++ code/SQLQuery.sql
@@ -0,0 +1,61 @@
+USE PinskiDatabase;
+
+
+-- Criando Dimensão Produto
+CREATE VIEW dProduto AS
+SELECT	
+	p.id AS 'produto_id',
+	p.descricao,
+	c.descricao AS categoria,
+	p.tamanho,
+	p.custoUnitario
+FROM Produto p 
+LEFT JOIN CategoriaProduto c
+	ON p.categoria_id = c.id;
+
+
+-- Criando Dimensão Cliente
+CREATE VIEW dCliente AS
+SELECT 
+	c.id AS 'cliente_id',
+	c.descricao,
+	g.cidade,
+	g.estado,
+	g.uf,
+	rb.Regiao
+FROM Cliente c
+LEFT JOiN Geografia g
+	ON c.geografia_id = g.id
+LEFT JOIN RegioesBrasil rb
+	ON g.uf = rb.UF;
+
+
+-- Criando Dimensão Vendedor
+CREATE VIEW dVendedor AS
+SELECT 
+	v.id AS 'vendedor_id',
+	v.descricao AS vendedor,
+	s.descricao AS supervisor,
+	s.gerente_descricao AS gerente
+FROM Vendedor v
+LEFT JOIN Supervisor s
+	ON v.supervisor_id = s.id;
+
+
+-- Criando Fato Vendas
+CREATE VIEW fVendas AS
+SELECT 
+	FORMAT(v.[data],'d', 'pt-BR') AS 'data',
+	v.nfe,
+	v.cliente_id,
+	cv.vendedor_id,
+	iv.produto_id,
+	iv.item_quantidade,
+	iv.valor_unitario,
+	v.nf_desconto * iv.valor_bruto / v.valor_bruto as 'valor_desconto'
+FROM Vendas v
+LEFT JOIN ItensVendas iv
+	ON v.id = iv.vendas_id
+LEFT JOIN ClientesVendedores cv
+	ON v.cliente_id = cv.cliente_id;
+
